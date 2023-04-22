7c6a600690ae59401cd3d0452bf34596dd2dce4c
prakash-15 <129299282+prakash-15@users.noreply.github.com>
2023-04-22 17:01:20 +0530

    Add files via upload

---
+++ week4/day3/CreateLead.java
@@ -0,0 +1,39 @@
+package week4.day3;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class CreateLead {
+@Test
+	public  void createLead () {
+		ChromeDriver driver  = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://leaftaps.com/opentaps/control/main");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.findElement(By.id("username")).sendKeys("Demosalesmanager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Create Lead")).click();
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("TestLeaf");
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Prakash");
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("Singh");
+		driver.findElement(By.id("createLeadForm_primaryPhoneNumber")).sendKeys("9843447673");
+		driver.findElement(By.name("submitButton")).click();
+		String text = driver.findElement(By.id("viewLead_companyName_sp")).getText();
+		if (text.contains("Testleaf")) {
+			System.out.println("Lead created successfully");
+		}
+		else {
+			System.out.println("Lead is not created");
+		}
+		driver.close();
+
+
+	}
+
+}
